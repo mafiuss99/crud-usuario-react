9f4edff5acbb81df703e3086e734ba8fa97bdf0f
Matheus Felipe <mf.mf98.23@gmail.com>
2021-09-26 17:00:55 -0300

    Criação de base do projeto, bem como o estado incial

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -8,7 +8,19 @@ const headerProps = {
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+const initialState = {
+    user: { name: '', email: '' },
+    list: []
+}
+
 export default class UserCrud extends Component {
+    state = { ...initialState }
+    
+    clear() {
+        this.setState({ user: initialState.user })
+    }
+
     render() {
         return (
             <Main {...headerProps}>
