7c4494864da622df2fe4025cebccdafecd74754f
Matheus Felipe <mf.mf98.23@gmail.com>
2021-09-26 17:42:48 -0300

    criação de bloco de cadastro de usuários

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -21,10 +21,86 @@ export default class UserCrud extends Component {
         this.setState({ user: initialState.user })
     }
 
+    save() {
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseUrl}/${user.id}`:baseUrl
+        axios[method](url, user)
+            .then(res => {
+                const list = this.getUpdateList(res.data)
+                this.setState({ user: initialState.user, list })
+            })
+    }
+
+    getUpdateList(user) {
+        const list = this.state.list.filter(u => u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
+    updateField(event) {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState( { user } )
+    }
+
+    renderForm() {
+        return (
+            <div className="form">
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>Nome</label>
+                            <input 
+                                type="text" 
+                                className="form-control"
+                                name="name"
+                                value={this.state.user.name}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o nome..."
+                            />
+                        </div>
+                    </div>
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                name="email" 
+                                value={this.state.user.email}
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o e-mail..."
+                            />
+                        </div>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button 
+                            className="btn btn-primary"
+                            onClick={e => this.save(e)}
+                        >
+                            Salvar
+                        </button>
+
+                        <button 
+                            className="btn btn-secondary ml-2"
+                            onClick={e => this.clear(e)}
+                        >
+                            Cancelar
+                        </button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <Main {...headerProps}>
-                Cadastro de Usuário
+                {this.renderForm()}
             </Main>
         )
     }
